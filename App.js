d7949603e693e229f2d7708d35d83a20f946f2af
Rogerio Filho <100595069+rogeriofilhoO@users.noreply.github.com>
2024-11-24 22:37:43 -0400

    Código finalizado do App

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+
+// Importando a biblioteca Express
 const express = require('express');
 
+// Importando as rotas
+const veiculosRotas = require('./routes/veiculos');
+const clientesRotas = require('./routes/clientes');
+
+// Colocando o express na constante 'app'
 const app = express();
 
+// Configurando o servidor
 const port = 8080; 
 const hostname = '127.0.0.1';
 
+// Configuração da rota da raiz
 app.get('/', (req, res) => {
     res.send("Você acesou a rota raíz.");
 });
 
+/* Configurando as rotas (get, put, delete, post) tanto do 
+'clientes' quanto do 'veículos'.*/
+app.use('/clientes', clientesRotas);
+app.use('/veiculos', veiculosRotas);
 
+//Fazendo o servidor funcionar
 app.listen(port, hostname, console.log('Servidor rodando...'));
